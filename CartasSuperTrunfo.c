79e584f8bf91e8a677d6e6e4374269506ff0a442
Enginer <diserteagle44@gmail.com>
2026-04-05 22:05:46 +0000

    Dando continuidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,50 +4,73 @@ int maim (){
 
 printf("\n-------- Carta 1 --------\n");
 char Inicial_do_Estado1;
-char Inicial_do_estado2;
-char Codigo_do_Estado1 [20];
-char Codigo_da_carta2 [50];
+char Codigo_da_carta1 [20];
 char Cidade1 [30];
-char Cidade2 [30];
 int Populacao1;
 float Area1;
 float PIB1;
-float PIB2;
 int Pontos_Turisticos1;
+printf("\n");
+char Inicial_do_estado2;
+char Codigo_da_carta2 [50];
+char Cidade2 [30];
+int Populacao2;
+float Area2;
+float PIB2;
 int Pontos_Turisticos2;
 
 printf("Inicial do estado1: \n");
-printf("Inicial do estado2: \n");
 scanf(" %c", &Inicial_do_Estado1);
-scanf(" %c", &Inicial_do_estado2);
 printf("\n");
+printf("Inicial do estado2: \n");
+scanf(" %c", &Inicial_do_estado2);
 printf("\n");
 
 printf("Código do Estado1: \n");
-scanf("%s", Codigo_do_Estado1);
+scanf("%s", Codigo_da_carta1);
+printf("\n");
+printf("Código do Estado2: \n");
+scanf("%s", Codigo_da_carta2);
 printf("\n");
 
 printf("Nome da cidade1: \n");
 scanf("%s", Cidade1);
 printf("\n");
+printf("Nome da cidade2: \n");
+scanf("%s", Cidade2);
+printf("\n");
 
 printf("Informe o número da populacional1: \n");
 scanf("%d", &Populacao1);
 printf("\n");
+printf("Informe o número da populacional2: \n");
+scanf("%d", &Populacao2);
+printf("\n");
+
 
 printf("Informe a área em km²1: \n");
 scanf("%d", &Area1);
+printf("\n");
+printf("Informe a área em km²2: \n");
+scanf("%d", &Area2);
 printf("\n");
 
 printf("Informe o PIB do estado1 em R$: \n");
 scanf("%f", &PIB1);
 printf("\n");
+printf("Informe o PIB do estado2 em R$: \n");
+scanf("%f", &PIB2);
+printf("\n");
 
 printf("Informe a quantidade de pontos turísticos1: \n");
 scanf("%d", &Pontos_Turisticos1);
 printf("\n");
+printf("Informe a quantidade de pontos turísticos2: \n");
+scanf("%d", &Pontos_Turisticos2);
+printf("\n");
 
-printf("\n------- Fim da carta 1-------\n");
+
+printf("\n------- Fim das cartas-------\n");
 
 return 0;
 
