bf9740fc475a50f98f92276b69cf905b14187731
Enginer <diserteagle44@gmail.com>
2026-04-05 21:49:16 +0000

    Meu código já estava pronto no vs code space. Estou transferindo para cá e implementando as duas cartas em uma só.
    Lembrando que os níveis básicos e aventureiros já estáo prontos.
    Estou organizando os dois antes de começar o nível mestre.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,56 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+int maim (){
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+printf("\n-------- Carta 1 --------\n");
+char Inicial_do_Estado1;
+char Inicial_do_estado2;
+char Codigo_do_Estado1 [20];
+char Codigo_da_carta2 [50];
+char Cidade1 [30];
+char Cidade2 [30];
+int Populacao1;
+float Area1;
+float PIB1;
+float PIB2;
+int Pontos_Turisticos1;
+int Pontos_Turisticos2;
 
-  // Área para entrada de dados
+printf("Inicial do estado1: \n");
+printf("Inicial do estado2: \n");
+scanf(" %c", &Inicial_do_Estado1);
+scanf(" %c", &Inicial_do_estado2);
+printf("\n");
+printf("\n");
 
-  // Área para exibição dos dados da cidade
+printf("Código do Estado1: \n");
+scanf("%s", Codigo_do_Estado1);
+printf("\n");
+
+printf("Nome da cidade1: \n");
+scanf("%s", Cidade1);
+printf("\n");
+
+printf("Informe o número da populacional1: \n");
+scanf("%d", &Populacao1);
+printf("\n");
+
+printf("Informe a área em km²1: \n");
+scanf("%d", &Area1);
+printf("\n");
+
+printf("Informe o PIB do estado1 em R$: \n");
+scanf("%f", &PIB1);
+printf("\n");
+
+printf("Informe a quantidade de pontos turísticos1: \n");
+scanf("%d", &Pontos_Turisticos1);
+printf("\n");
+
+printf("\n------- Fim da carta 1-------\n");
 
 return 0;
-} 
+
+
+
+}
